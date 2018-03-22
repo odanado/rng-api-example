6e16caff18cad9a15d7675db4f7ad18d0b1e203a
odan <odan3240@gmail.com>
2018-03-23 02:19:42 +0900

    add

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <RngApiExample/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import RngApiExample from './components/RngApiExample';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    RngApiExample,
   },
 };
 </script>
@@ -23,6 +22,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 80%;
+  margin: 0 auto;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/RngApiExample.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div>
+      <button
+        v-for="(_, i) in 17"
+        :key="i"
+        @click="needles.push(i)">
+        {{ i }}
+      </button>
+    </div>
+
+    <select v-model="selectedSearchMode">
+      <option
+        v-for="searchMode in searchModes"
+        :key="searchMode">
+        {{ searchMode }}
+      </option>
+    </select>
+    <p>needles: {{ needles }}</p>
+    <button @click="fetchSeed()">
+      send
+    </button>
+    <ul id="results">
+      <li
+        v-for="result in results"
+        :key="result.seed">
+        {{ result.seed }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      needles: [],
+      searchModes: ['usm/sfmt/seed', 'sm/sfmt/seed'],
+      selectedSearchMode: 'usm/sfmt/seed',
+      results: [],
+    };
+  },
+  methods: {
+    async fetchSeed() {
+      const url = `https://rng-api.odanado.com/${this.selectedSearchMode}`;
+      const response = await axios.get(url, {
+        params: { needle: this.needles.join() },
+      });
+      this.results = response.data.results;
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+}
+</style>
